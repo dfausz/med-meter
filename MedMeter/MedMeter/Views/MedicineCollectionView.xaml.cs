9ef8ddbe32ed22ef14682830769defd5aa3ad981
dfausz <dbfausz@gmail.com>
2021-09-12 21:53:41 -0500

    Adds OverflowMenu

--- MedMeter/MedMeter/Views/MedicineCollectionView.xaml.cs
+++ MedMeter/MedMeter/Views/MedicineCollectionView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MedMeter.ViewModels;
 using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MedMeter.Views
@@ -15,9 +16,33 @@ namespace MedMeter.Views
             ViewModel = new MedicineCollectionViewModel();
             BindingContext = ViewModel;
         }
+
         private void Button_Clicked(object sender, EventArgs e)
         {
             ViewModel.TakeMedicine((sender as Button).CommandParameter as MedicineViewModel);
+        }
+
+        private void SwipeItem_Invoked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            ButtonColorFlash(sender as Label);
+        }
+
+        private async void ButtonColorFlash(View view)
+        {
+            var originalColor = view.BackgroundColor;
+            view.BackgroundColor = Color.LightGray;
+            await Task.Delay(500);
+            view.BackgroundColor = originalColor;
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            var layout = sender as StackLayout;
         }
     }
 }
